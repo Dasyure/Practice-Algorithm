9fc6b9ef97c02c73004e07bf1a254e2aa50ea6c4
haesun <haesunsw@gmail.com>
2020-05-04 02:05:22 +1000

    TreeCopy

--- test.c
+++ test.c
@@ -1,8 +1,14 @@
-
 #include "tree.h"
-
 Tree TreeCopy(Tree t, int depth) {
-	// TODO
-	return NULL;
+	if (t == NULL || depth < 0) {
+	    return NULL;
+    }  
+	else {
+	    Tree copied_tree = malloc(sizeof(struct node));
+        copied_tree->value = t->value;
+        copied_tree->left = TreeCopy(t->left, depth - 1);
+        copied_tree->right = TreeCopy(t->right, depth - 1);
+        return copied_tree;
+    }
 }
 
