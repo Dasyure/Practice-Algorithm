cf0e4d087a3c11aa7983006eef232f7e150849a8
haesun <haesunsw@gmail.com>
2020-05-04 02:31:50 +1000

    TreeSumOdds

---
+++ TreeSumOdds.c
@@ -0,0 +1,17 @@
+
+#include <stdlib.h>
+
+#include "tree.h"
+
+int TreeSumOdds(Tree t) {
+
+	if (t == NULL) {
+		return 0;
+	}
+	if (t->value % 2 != 0) {
+		return t->value + TreeSumOdds(t->left) + TreeSumOdds(t->right);
+	}
+
+	return TreeSumOdds(t->left) + TreeSumOdds(t->right);
+}
+
