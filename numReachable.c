b632967888f57b25abe02c5d8b9dfa27d794a87e
haesun <haesunsw@gmail.com>
2020-05-04 18:39:52 +1000

    without declaring graph struct

--- numReachable.c
+++ numReachable.c
@@ -3,36 +3,31 @@
 #include <stdlib.h>
 
 #include "Graph.h"
-struct graph {
-	int nV;
-	bool **edges;
-};
-
 
 int numReachable(Graph g, int src) {
 	// TODO
 
 	int i = 0;
 	int j = 0;
-	int visited[g->nV];
+	int visited[GraphNumVertices(g)];
 	// first you make every vertices unvisited
-    for (i = 0; i < g->nV; i++) {
+    for (i = 0; i < GraphNumVertices(g); i++) {
 		visited[i] = -1; 
 	}
 	i = 0;
 	visited[src] = 2;
 	int count = 1;
-	while (i < g->nV) {
+	while (i < GraphNumVertices(g)) {
 		
-	    if (g->edges[src][i]) {
+	    if (GraphIsAdjacent(g, src, i)) {
 			if (visited[i] != 2) {
 				visited[i] = 2;
 				count++;
 				//printf("how come\n");
 			}
 			j = 0;
-			while (j < g->nV) {
-				if (g->edges[i][j] && visited[j] != 2) {
+			while (j < GraphNumVertices(g)) {
+				if (GraphIsAdjacent(g, i, j) && visited[j] != 2) {
 					//printf("i:%d, j:%d\n", i, j);
 					visited[j] = 2;
 					count++;
